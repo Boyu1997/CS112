8eec35cb816d8f4b864d4f7cdd4f04194b9109d2
Boyu Jiang <jbyjiangboyu@126.com>
2018-03-03 15:43:12 +0530

    Post Session_2_2

--- Session_2_2/Session_2_2.R
+++ Session_2_2/Session_2_2.R
@@ -1,6 +1,19 @@
 setwd("/Users/boyu/Documents/Minerva/Second\ Year/Sesond\ Semester/CS112/R\ Code/Session_2_2")
-getwd()
 
-install.packages("foreign")
-library("foreign")
-load.Rdata("haha.RData")
+
+load("haha.RData")
+haha <- data.frame(post.test, pre.test, treatment)
+
+
+# treatment effect (as we have both Y0 and Y1)
+subset.control = subset(haha, treatment==0)
+subset.treated = subset(haha, treatment==1)
+# average treatement effect for the control
+mean(subset.control$post.test - subset.control$pre.test)
+# average treatement effect for the treated
+mean(subset.treated$post.test - subset.treated$pre.test)
+
+
+# linear regression model
+lm <- lm(post.test ~ pre.test + treatment, data=haha)
+summary(lm)
